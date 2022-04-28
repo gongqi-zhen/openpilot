5877383f3d375717e165a1df1cd0ca575f7bef42
nuwandavek <vivekaithal44@gmail.com>
2022-04-27 17:13:53 -0700

    add rotation arm: center-to-front

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -18,7 +18,7 @@ class LateralPlanner:
 
     self.last_cloudlog_t = 0
     self.steer_rate_cost = CP.steerRateCost
-    self.r = CP.wheelbase
+    self.r = CP.centerToFront
     self.solution_invalid_cnt = 0
 
     self.path_xyz = np.zeros((TRAJECTORY_SIZE, 3))
